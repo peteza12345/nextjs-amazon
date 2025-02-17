d355e29be96b29f8813d5bc4d56125cb45be9c0a
peteza12345 <pete-e-2012@hotmail.com>
2025-02-17 21:06:07 +0700

    create-setting

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,29 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+
 import qs from "query-string";
+
+export function formUrlQuery({
+  params,
+  key,
+  value,
+}: {
+  params: string;
+  key: string;
+  value: string | null;
+}) {
+  const currentUrl = qs.parse(params);
+
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+}
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -10,6 +33,8 @@ export const formatNumberWithDecimal = (num: number): string => {
   const [int, decimal] = num.toString().split(".");
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : int;
 };
+// PROMPT: [ChatGTP] create toSlug ts arrow function that convert text to lowercase, remove non-word,
+// non-whitespace, non-hyphen characters, replace whitespace, trim leading hyphens and trim trailing hyphens
 
 export const toSlug = (text: string): string =>
   text
@@ -24,13 +49,11 @@ const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
   style: "currency",
   minimumFractionDigits: 2,
 });
-
 export function formatCurrency(amount: number) {
   return CURRENCY_FORMATTER.format(amount);
 }
 
 const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
-
 export function formatNumber(number: number) {
   return NUMBER_FORMATTER.format(number);
 }
@@ -48,19 +71,15 @@ export const formatError = (error: any): string => {
       const errorMessage = error.errors[field].message;
       return `${error.errors[field].path}: ${errorMessage}`; // field: errorMessage
     });
-
     return fieldErrors.join(". ");
   } else if (error.name === "ValidationError") {
     const fieldErrors = Object.keys(error.errors).map((field) => {
       const errorMessage = error.errors[field].message;
-
       return errorMessage;
     });
-
     return fieldErrors.join(". ");
   } else if (error.code === 11000) {
     const duplicateField = Object.keys(error.keyValue)[0];
-
     return `${duplicateField} already exists`;
   } else {
     // return 'Something went wrong. please try again'
@@ -75,23 +94,22 @@ export function calculateFutureDate(days: number) {
   currentDate.setDate(currentDate.getDate() + days);
   return currentDate;
 }
+export function getMonthName(yearMonth: string): string {
+  const [year, month] = yearMonth.split("-").map(Number);
+  const date = new Date(year, month - 1);
+  const monthName = date.toLocaleString("default", { month: "long" });
+  const now = new Date();
 
-export function getMonthName(yearAndMonth: string) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [year, monthNumber] = yearAndMonth.split("-");
-  const date = new Date();
-  date.setMonth(parseInt(monthNumber) - 1);
-  return new Date().getMonth() === parseInt(monthNumber) - 1
-    ? `${date.toLocaleString("default", { month: "long" })} (ongoing)`
-    : date.toLocaleString("default", { month: "long" });
+  if (year === now.getFullYear() && month === now.getMonth() + 1) {
+    return `${monthName} Ongoing`;
+  }
+  return monthName;
 }
-
 export function calculatePastDate(days: number) {
   const currentDate = new Date();
   currentDate.setDate(currentDate.getDate() - days);
   return currentDate;
 }
-
 export function timeUntilMidnight(): { hours: number; minutes: number } {
   const now = new Date();
   const midnight = new Date();
@@ -136,7 +154,6 @@ export const formatDateTime = (dateString: Date) => {
     "en-US",
     timeOptions
   );
-
   return {
     dateTime: formattedDateTime,
     dateOnly: formattedDate,
@@ -146,28 +163,6 @@ export const formatDateTime = (dateString: Date) => {
 
 export function formatId(id: string) {
   return `..${id.substring(id.length - 6)}`;
-}
-
-export function formUrlQuery({
-  params,
-  key,
-  value,
-}: {
-  params: string;
-  key: string;
-  value: string | null;
-}) {
-  const currentUrl = qs.parse(params);
-
-  currentUrl[key] = value;
-
-  return qs.stringifyUrl(
-    {
-      url: window.location.pathname,
-      query: currentUrl,
-    },
-    { skipNull: true }
-  );
 }
 
 export const getFilterUrl = ({
@@ -202,6 +197,5 @@ export const getFilterUrl = ({
   if (rating) newParams.rating = rating;
   if (page) newParams.page = page;
   if (sort) newParams.sort = sort;
-
   return `/search?${new URLSearchParams(newParams).toString()}`;
 };
